4b44102d5523da9675ccd06b41721a5c24160e86
javier <javvierel2006@hotmail.com>
2025-11-06 21:27:47 -0500

    cambio

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HojaVidaApp;
 using HojaVidaApp.Services;
 
-
+/*444*/
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
